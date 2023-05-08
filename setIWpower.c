dc05b00c515f5cd52c1cf96609c47daedccbc678
Kacper Nadstoga <kapix007@gmail.com>
2023-05-08 10:34:31 +0200

    refactor to pass only char pointer to function

--- setIWpower.c
+++ setIWpower.c
@@ -8,33 +8,33 @@
 #include <linux/wireless.h>
 #include <sys/ioctl.h>
 
-char getSSID(int socketDescriptor, char* interfaceName){
-
-    struct iwreq wireless_req;
-    char ssid[IW_ESSID_MAX_SIZE + 1];
-
-    memset(&wireless_req, 0, sizeof(struct iwreq));
-    memset(ssid, 0, sizeof(ssid));
-    wireless_req.u.essid.pointer = ssid;
-    wireless_req.u.essid.length = IW_ESSID_MAX_SIZE;
-    iwreq.u.essid.flags = 0;
+// procedure to get ssid of IW card - pass socket, interface name, ssid, and wireless request
+void getSSID(int socketDescriptor, char* interfaceName, char* ssid, struct iwreq wireless_req){
 
     strncpy(wireless_req.ifr_name, interfaceName, IFNAMSIZ);
 
     if(ioctl(socketDescriptor, SIOCGIWESSID, &wireless_req) == -1){
         perror("ioctl");
-        close(socketDescriptor)
-        return "\0";
+        close(socketDescriptor);
     }
 
     strncpy(ssid, wireless_req.u.essid.pointer, sizeof(ssid));
-    return ssid;
 }
 
 
 int main(int argc, char** argv){
+
+    struct iwreq wireless_req;
     int descriptor;
     char ssid[IW_ESSID_MAX_SIZE + 1];
+
+
+    memset(&wireless_req, 0, sizeof(struct iwreq));
+    memset(ssid, 0, sizeof(ssid));
+    wireless_req.u.essid.pointer = ssid;
+    wireless_req.u.essid.length = IW_ESSID_MAX_SIZE;
+    wireless_req.u.essid.flags = 0;
+
     
     memset(ssid, 0, sizeof(ssid));
 
@@ -43,7 +43,10 @@ int main(int argc, char** argv){
         perror("socket");
         return -1;
     }
-    ssid = getSSID(descriptor, argv[1]);
+
+    // run function to get ssid of IW card through ioctl
+    getSSID(descriptor, argv[1], ssid ,wireless_req);
+
     if(ssid != "\0")
         printf("SSID: %s\n", ssid);
     else
