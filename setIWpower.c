b2898572d8369c50a21d718f9c8d196a09bf2b12
Kacper Nadstoga <kapix007@gmail.com>
2023-05-11 21:22:06 +0200

    create util to create descriptor and getter for frequency

--- setIWpower.c
+++ setIWpower.c
@@ -11,104 +11,106 @@
 
 #define IW_MAX_POWER_POLAND_DBM 20
 
-// procedure to get ssid of IW card - pass socket, interface name, ssid, and wireless request
+// procedure to get ssid of IW card - interface name, ssid, and wireless request
 void getSSID(int socketDescriptor, char* ssid, struct iwreq wireless_req);
 
-// procedure to get power of IW card - pass socket, power, and wireless request
-void getIWPower(int socketDescriptor ,char* powerDbm, struct iwreq wireless_req);
+// procedure to get power of IW card - power, and wireless request
+void getIWPower(char* powerDbm, struct iwreq wireless_req);
 
-// procedure to set power of IW card - pass socket, power in dBM, and wireless request
-void setIWPower(int socketDescriptor, int power, struct iwreq wireless_req);
+// procedure to set power of IW card - power in dBM, and wireless request
+void setIWPower(int power, struct iwreq wireless_req);
 
 // procedure to check power if user can change
 bool checkAllowedPower(int power);
 
-void adaptiveMode(int descriptor,char* power ,struct iwreq wireless_req);
+// procedure to adaptive mode of IW card controlling
+void adaptiveMode(char* power, struct iwreq wireless_req);
 
+// utils function to create new descriptor
+int createDescriptor();
+
+// get frequency of IW card
+void getIWFrequency(struct iwreq wireless_req);
 
 
 int main(int argc, char** argv){
 
+    // declaration of structure to operate on Wireless interface and local variables
     struct iwreq wireless_req;
     int descriptor;
     char ssid[IW_ESSID_MAX_SIZE + 1];
     char powerDbm[100];
 
-
+    // reset variables
     memset(&wireless_req, 0, sizeof(struct iwreq));
     memset(ssid, 0, sizeof(ssid));
+    memset(powerDbm, 0, sizeof(powerDbm));
+
+    // fill structure
     wireless_req.u.essid.pointer = ssid;
     wireless_req.u.essid.length = IW_ESSID_MAX_SIZE;
     wireless_req.u.essid.flags = 0;
 
-    
-    memset(ssid, 0, sizeof(ssid));
+    descriptor = createDescriptor();
 
-    descriptor = socket(PF_INET, SOCK_DGRAM, 0);
     if(descriptor == -1){
         perror("socket");
         return -1;
     }
 
+    //pass interface name to structure
     strncpy(wireless_req.ifr_name, argv[1], IFNAMSIZ);
 
     // run function to get ssid of IW card through ioctl
     getSSID(descriptor, ssid ,wireless_req);
 
-   
-
-    if(ssid != "\0" && powerDbm !="\0"){
+    if(strlen(ssid) != 0){
         printf("SSID: %s\n", ssid);
 
-        // run function to get power of IW card through ioctl
-        getIWPower(descriptor, powerDbm,wireless_req);
+        // run function to get power and frequency of IW card through ioctl
+        getIWPower(powerDbm,wireless_req);
+        getIWFrequency(wireless_req);
     }else{
         printf("SSID: Not found!\n");
         close(descriptor);
+        descriptor = -1;
         return -1;
     }
-    // setIWPower(descriptor, 18, wireless_req);
-    // getIWPower(descriptor, powerDbm, wireless_req);
-    // printf("Actual power of IW card: %s[dBm]\n", powerDbm);
     
-   while (1)
-    {     
-        printf("What do you want to do? \n");
-        printf("1. Set power card manually -> type 's'\n");
-        printf("2. Adaptive card monitor mode -> type 'a'\n");
-        printf("3. Exit -> type 'e'\n");
+    printf("What do you want to do? \n");
+    printf("1. Set power card manually -> type 's'\n");
+    printf("2. Adaptive card monitor mode -> type 'a'\n");
+    printf("3. Exit -> type 'e'\n");
+    printf("========================================\n");
 
-        char userChoice;
-        printf("Enter your choice: ");
-        scanf("%c", &userChoice);
+    char userChoice;
+    printf("Enter your choice: ");
+    scanf("%c", &userChoice);
 
-        switch (userChoice)
-        {
-            case 's':{
-                int power;
-                printf("Enter power in dBm: ");
+    switch (userChoice){
+        case 's':{
+            int power;
                 scanf("%d", &power);
-                if(checkAllowedPower(power)){
-                    setIWPower(descriptor, power, wireless_req);
-                    getIWPower(descriptor, powerDbm, wireless_req);
-                }else{
-                    printf("Power is too high!\n");
-                }
-                break;
+            if(checkAllowedPower(power)){
+                setIWPower(power, wireless_req);
+                getIWPower(powerDbm, wireless_req);
+            }else{
+                printf("Power is too high!\n");
             }
-
-            case 'a':{
-                adaptiveMode(descriptor,powerDbm ,wireless_req);
-                break;
-            }
-    
-            case 'e':
-            exit(1);
             break;
         }
-    }
-    
 
+        case 'a':{
+            adaptiveMode(powerDbm ,wireless_req);
+            break;
+        }
+
+        case 'e':
+        exit(1);
+        break;
+    }
+    printf("========================================\n");
+    
     close(descriptor);
     return EXIT_SUCCESS;
 }
@@ -123,7 +125,13 @@ void getSSID(int socketDescriptor, char* ssid, struct iwreq wireless_req){
     strncpy(ssid, wireless_req.u.essid.pointer, sizeof(ssid));
 }
 
-void getIWPower(int socketDescriptor, char* powerDbm, struct iwreq wireless_req){
+void getIWPower(char* powerDbm, struct iwreq wireless_req){
+
+    int socketDescriptor = createDescriptor();
+    if(socketDescriptor == -1){
+        perror("socket");
+        return;
+    }
     
     wireless_req.u.txpower.flags = IW_TXPOW_DBM;
     wireless_req.u.txpower.fixed = 0;
@@ -139,9 +147,18 @@ void getIWPower(int socketDescriptor, char* powerDbm, struct iwreq wireless_req)
 
     strncpy(powerDbm, powerInStr, sizeof(powerDbm));
     printf("Power IW card: %s[dBm]\n", powerDbm);
+    return;
 }
 
-void setIWPower(int socketDescriptor, int power, struct iwreq wireless_req){
+void setIWPower(int power, struct iwreq wireless_req){
+
+    int socketDescriptor = createDescriptor();
+
+    if(socketDescriptor == -1){
+        perror("socket");
+        return;
+    }
+
     wireless_req.u.txpower.value = power;
     wireless_req.u.txpower.fixed = 1;
     wireless_req.u.txpower.disabled = 0;
@@ -151,7 +168,36 @@ void setIWPower(int socketDescriptor, int power, struct iwreq wireless_req){
         perror("ioctl");
         close(socketDescriptor);
     }
-    printf("Power set to %d dBm\n", power);
+}
+
+
+void adaptiveMode(char* power, struct iwreq wireless_req){
+
+    int socketDescriptor = createDescriptor();
+
+    if(socketDescriptor == -1){
+        perror("socket");
+        return;
+    }
+
+    int userChoice = 0;
+    printf("Enter min value: ");
+    scanf("%d", &userChoice);
+
+    printf("Adaptive mode started.\n");
+    printf("========================================\n");
+    
+    while(1){
+        
+        sleep(5);
+        getIWPower(power, wireless_req);
+
+        if(atoi(power) < userChoice || atoi(power) == IW_MAX_POWER_POLAND_DBM -1 ){
+
+            printf("I have to power up.\n");
+            setIWPower(atoi(power) + 1, wireless_req);
+        }
+    }
 }
 
 bool checkAllowedPower(int power){
@@ -161,22 +207,29 @@ bool checkAllowedPower(int power){
     return true;
 }
 
-void adaptiveMode(int descriptor,char* power, struct iwreq wireless_req){
-    int userChoice = 0;
-    printf("Enter min value: ");
-    scanf("%d", &userChoice);
-    
-    while(1){
-        
-        sleep(5);
-        getIWPower(descriptor, power, wireless_req);
 
-        if(atoi(power) < userChoice && atoi(power) == IW_MAX_POWER_POLAND_DBM -1 ){
+int createDescriptor(){
+    int descriptor = socket(AF_INET, SOCK_DGRAM, 0);
+    return descriptor;
+}
 
-            printf("I have to power up.\n");
-            setIWPower(descriptor, atoi(power) + 1, wireless_req);
+void getIWFrequency(struct iwreq wireless_req){
 
-        }
+    int socketDescriptor = createDescriptor();
+    if(socketDescriptor == -1){
+        perror("socket");
+        return;
     }
+
+    char powerInStr[100];
+    
+    if(ioctl(socketDescriptor, SIOCGIWFREQ, &wireless_req) == -1){
+        perror("ioctl");
+        close(socketDescriptor);
+    }
+    snprintf(powerInStr, sizeof(powerInStr), "%d", wireless_req.u.freq.m);
+
+    // strncpy(powerDbm, powerInStr, sizeof(powerDbm));
+    printf("Frequency IW card: %s[GHz]\n", powerInStr);
     return;
 }
